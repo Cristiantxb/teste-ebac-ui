86c572758327307a0844d8cf40991d33d783a72e
Cristian Borges <cristian_teixeirab@hotmail.com>
2024-06-10 11:05:29 -0300

    selecionar produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')    
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+
+        cy.get('.row')
+        //.first()
+        //.last()
+        //.eq(2)
+        .contains('Aero Daily Fitness Tee')
+        .click()
+
+        cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+
+        
+    });
+});
