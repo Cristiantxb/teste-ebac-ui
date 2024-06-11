3c3853570f2b0b8cc6ba022e4bb047592b6af4a2
Cristian Borges <cristian_teixeirab@hotmail.com>
2024-06-11 10:50:02 -0300

    arquivo de dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress"/>
+const perfil = require ('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/') 
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
-        cy.screenshot()
+        //cy.screenshot()
     });
     it('Deve fazer login com sucesso', () => {    
         cy.get('#username').type('cristian.txb@teste.com.br')
@@ -29,6 +30,24 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail cristian.txb@teste.com.br está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cristian.txb (não é cristian.txb? Sair)')
+        
+    });
+
+    it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then(dados=>{
+            cy.get('#username').type(dados.usuario , {log: false})
+            cy.get('#password').type(dados.senha , {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cristian.txb (não é cristian.txb? Sair)')    
+        })
+        
     });
 
 })
