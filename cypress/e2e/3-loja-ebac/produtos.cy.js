93321387198724cd99c412231f477d8cc5da452d
Cristian Borges <cristian_teixeirab@hotmail.com>
2024-06-13 10:38:57 -0300

    alteração produtos

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -13,17 +13,35 @@ describe('Funcionalidade: Produtos', () => {
             cy.get('#tab-title-description > a').should('contain' , 'Descrição')       
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Beaumont Summit Kit'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Aero Daily Fitness Tee')
+        cy.get('.product_title').should('contain', 'Aero Daily Fitness Tee')
     });
 
     it('Deve adicionar produto ao carrinho ', () => {
-        
+        let qtd = 10
+        produtosPage.buscarProduto('Zoltan Gym Tee')
+        produtosPage.addProdutoCarrinho('M', 'Blue', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Zoltan Gym Tee” foram adicionados no seu carrinho.')
     });
+
+    it.only('Deve adicionar produto ao carrinho - buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados =>{
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+            dados[0].tamanho,
+            dados[0].cor,
+            dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)    
+        })
+
+
+    });
+
 });
